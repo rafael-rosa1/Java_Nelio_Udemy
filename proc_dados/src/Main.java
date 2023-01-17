d0d2f8e398de84f3880037a96f6d525768a84344
Rafael Rosa <rafael.f.rosa1@gmail.com>
2023-01-16 22:19:45 -0300

    proc de dados e casting

---
+++ proc_dados/src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        int x, y;
+//        x = 5;
+//        y = 2 * x;
+
+//        int x;
+//        double y;
+//
+//        x = 5;
+//        y = 2 * x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+//        double b, B, h, area;
+//
+//        b = 6.0;
+//        B = 8.0;
+//        h = 5.0;
+//
+//        area = (b + B) / 2.0 * h;
+//        System.out.println(area);
+
+//        int a, b;
+//        double resultado;
+//
+//        a = 5;
+//        b = 2;
+//
+//        resultado = (double) a / b; //casting: conversao explicita dos valores
+//
+//        System.out.println(resultado);
+
+        double a;
+        int b;
+        a = 5.0;
+        b = (int) a; //casting: conversao explicita dos valores
+        System.out.println(b);
+    }
+}
