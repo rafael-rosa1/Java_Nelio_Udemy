abb8c8d4d7827b0e1986108ccbcadada9a95d536
Rafael Rosa <rafael.f.rosa1@gmail.com>
2023-01-16 22:09:02 -0300

    saida de dados

---
+++ saida_de_dados/src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        int y = 32;
+//        System.out.println(y);
+//
+//        double x = 10.35784;
+//        System.out.println(x);
+//        System.out.printf("%.2f%n", x);
+//        Locale.setDefault(Locale.US); //usa separador de decimais dos EUA
+//        System.out.printf("%.4f%n", x);
+//
+//        System.out.println("Bom dia!");
+//
+//        System.out.println("RESULTADO = " + x + " METROS");
+//        System.out.printf("RESULTADO = %.2f metros%n", x);
+//
+//        String nome = "Maria";
+//        int idade = 31;
+//        double renda = 4000.0;
+//        System.out.printf("%s tem %d anos e ganha R$ %.2f reais", nome, idade, renda);
+//
+        // exercicio
+        String product1 = "Computer";
+        String product2 = "Office desk";
+
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.234567;
+
+        System.out.println("Products:");
+        System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+        System.out.printf("%s, which price is $ %.2f%n%n", product2, price2);
+
+        System.out.printf("%d years old, code %d and gender: %s%n%n", age, code, gender);
+
+        System.out.printf("Measure with eight decimal places: %.8f%n", measure);
+        System.out.printf("Rounded (three decimal places): %.3f%n", measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal point: %.3f", measure);
+    }
+}
