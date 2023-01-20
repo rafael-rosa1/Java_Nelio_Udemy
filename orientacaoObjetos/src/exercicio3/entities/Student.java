0fa43cf0274967f5707ca1f146e12aa57f083b9e
Rafael Rosa <rafael.f.rosa1@gmail.com>
2023-01-20 20:29:46 -0300

    exercicio3

---
+++ orientacaoObjetos/src/exercicio3/entities/Student.java
@@ -0,0 +1,22 @@
+package exercicio3.entities;
+
+public class Student {
+    public String name;
+    public double g1;
+    public double g2;
+    public double g3;
+
+    public double finalGrade() {
+        return g1 + g2 + g3;
+    }
+
+    public void approved() {
+        if(finalGrade() >= 60.0) {
+            System.out.println("PASS");
+        } else {
+            double missing = 60.0 - finalGrade();
+            System.out.println("FAILED");
+            System.out.println("MISSING " + missing + " POINTS");
+        }
+    }
+}
