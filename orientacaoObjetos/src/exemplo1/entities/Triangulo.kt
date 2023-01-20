c5a6fa8fb74a2b4e4c39b623e3145626a10b3801
Rafael Rosa <rafael.f.rosa1@gmail.com>
2023-01-19 21:08:42 -0300

    exemplo1 orientacao a objetos

---
+++ orientacaoObjetos/src/exemplo1/entities/Triangulo.kt
@@ -0,0 +1,14 @@
+package exemplo1.entities
+
+import kotlin.math.sqrt
+
+class Triangulo {
+    var a: Double = 0.0
+    var b: Double = 0.0
+    var c: Double = 0.0
+
+    fun area(): Double {
+        val p = (a + b + c) / 2.0
+        return sqrt(p * (p - a) * (p - b) * (p - c))
+    }
+}
