2748dde79f55217c26dabad253d0458d1b88d70f
Rafael Rosa <rafael.f.rosa1@gmail.com>
2023-02-14 10:45:11 -0300

    exercicio numeros pares

---
+++ memoriaArraysListas/src/vetores/exercicios/exercicio4/Program.java
@@ -0,0 +1,35 @@
+package vetores.exercicios.exercicio4;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos numeros você vai digitar? ");
+        int n = sc.nextInt();
+
+        int[] vect = new int[n];
+
+        for (int i = 0; i < vect.length; i++) {
+            System.out.print("Digite um numero: ");
+            vect[i] = sc.nextInt();
+        }
+
+        int totalPares = 0;
+        System.out.println("NUMEROS PARES:");
+        for (int i = 0; i < vect.length; i++) {
+            if(vect[i] % 2 == 0) {
+                System.out.print(vect[i] + " ");
+                totalPares++;
+            }
+        }
+        System.out.println();
+        System.out.print("QUANTIDADE DE PARES = ");
+        System.out.println(totalPares);
+        sc.close();
+    }
+}
